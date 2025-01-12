846acc1c490f56ab6f32a7d43066ed216c5c564b
Yo <franjomor@gmail.com>
2025-01-12 13:55:06 +0100

    feat: product service

--- git/shopping/src/main/java/com/oreilly/shopping/config/AppInit.java
+++ git/shopping/src/main/java/com/oreilly/shopping/config/AppInit.java
@@ -1,33 +1,20 @@
 package com.oreilly.shopping.config;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import com.oreilly.shopping.dao.ProductRepository;
-import com.oreilly.shopping.entities.Product;
+import com.oreilly.shopping.services.ProductService;
 
 @Component
 public class AppInit implements CommandLineRunner {
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public AppInit(ProductRepository productRepository){
-        this.productRepository = productRepository;
+    public AppInit(ProductService productService){
+        this.productService = productService;
     }
 
     @Override
     public void run(String... args){
-        if(productRepository.count() == 0){
-            productRepository.saveAll(
-                List.of(
-                    new Product("Tv tray", BigDecimal.valueOf(4.95)),
-                    new Product("Toaster", BigDecimal.valueOf(19.95)),
-                    new Product("Sofa", BigDecimal.valueOf(249.95))
-                )
-            ).forEach(System.out::println);
-        }
+        productService.initializeDatabase();
     }
     
 }
---
+++ git/shopping/src/main/java/com/oreilly/shopping/services/ProductService.java
@@ -0,0 +1,32 @@
+package com.oreilly.shopping.services;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oreilly.shopping.dao.ProductRepository;
+import com.oreilly.shopping.entities.Product;
+
+@Service
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public void initializeDatabase(){
+        if(productRepository.count() == 0){
+            productRepository.saveAll(
+                List.of(
+                    new Product("Tv tray", BigDecimal.valueOf(4.95)),
+                    new Product("Toaster", BigDecimal.valueOf(19.95)),
+                    new Product("Sofa", BigDecimal.valueOf(249.95))
+                )
+            ).forEach(System.out::println);
+        }
+    }
+}
