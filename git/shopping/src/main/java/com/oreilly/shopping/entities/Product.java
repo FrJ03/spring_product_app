e325d8143b34f311e21f2124cb630958e5f1b38e
Yo <franjomor@gmail.com>
2025-01-12 13:39:31 +0100

    feat: adding validations to product attributes

--- git/shopping/src/main/java/com/oreilly/shopping/entities/Product.java
+++ git/shopping/src/main/java/com/oreilly/shopping/entities/Product.java
@@ -7,12 +7,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @Entity
 public class Product {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+    
+    @PositiveOrZero(message = "Price must be zero or greater")
     private BigDecimal price;
 
     public Product(){}
